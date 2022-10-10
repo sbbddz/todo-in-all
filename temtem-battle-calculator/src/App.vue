eb5b50b750cb5dc3e5a6700404d790cd5b66b6fc
mrtnmmn <mrtn.lmba@gmail.com>
2022-10-10 21:09:11 +0200

    visual changes

--- temtem-battle-calculator/src/App.vue
+++ temtem-battle-calculator/src/App.vue
@@ -1,16 +1,45 @@
 <template>
-  <MainCalculator/>
+  <div class="comboDiv">
+    <h1>Attack type:</h1>
+    <TypeSelector v-model="attack"></TypeSelector>
+  </div>
+
+  <div class="comboDiv">
+    <h1>Defense type:</h1>
+    <TypeSelector v-model="defense1"></TypeSelector>
+  </div>
+
+  <div class="comboDiv" v-if="defense1 != 1">
+    <h1>Defense type 2:</h1>
+    <TypeSelector v-model="defense2"></TypeSelector>
+  </div>
 </template>
 
 <script>
-import MainCalculator from './components/MainCalculator'
+import TemTemTypes from './Assets/Types.json'
+import TypeSelector from "@/components/TypeSelector";
+import {ref} from "vue";
 
 export default {
   name: 'App',
+  temtemTypes: TemTemTypes,
   components: {
-    MainCalculator,
-  }
-}
+    TypeSelector,
+  },
+  props:{
+    temtemTypes: TemTemTypes,
+  },
+  setup() {
+    const attack = ref(1);
+    const defense1 = ref(1);
+    const defense2 = ref(1);
+    return {
+      attack,
+      defense1,
+      defense2,
+    };
+  },
+};
 </script>
 
 <style>
@@ -18,11 +47,18 @@ export default {
   display: flex;
   min-height: 100vh;
   min-width: 100vw;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 }
 
 body {
   margin: 0px;
+}
+
+.comboDiv {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
 }
 </style>
---
+++ temtem-battle-calculator/src/components/TypeSelector.vue
@@ -0,0 +1,50 @@
+<template>
+  <fieldset>
+    <div class="input-group group-sm mb-2">
+      <span class="input-group-text" v-html="label"></span>
+      <select
+          class="form-control"
+          :value="modelValue"
+          @change="$emit('update:modelValue', $event.target.value)"
+      >
+        <option v-for="type in typesArray" :value="type">
+          {{type}}
+        </option>
+      </select>
+    </div>
+    <p>SelectedField model value: {{ modelValue }}</p>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: "TypeSelector",
+  data() {
+    return {
+      typesArray: [
+        "not selected",
+        "normal",
+        "fire",
+        "water",
+        "nature",
+        "electric",
+        "earth",
+        "mental",
+        "wind",
+        "digital",
+        "melee",
+        "cristal",
+        "toxic"
+      ]
+    }
+  },
+  props: {
+    modelValue: String,
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
